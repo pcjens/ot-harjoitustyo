f8bb70585855a182f4a90fa57ebda53a5528d2ec
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2019-03-23 23:51:37 +0200

    Add unicafe tests

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,37 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanLataaminenToimii() {
+        kortti.lataaRahaa(100);
+        assertEquals("saldo: 1.10", kortti.toString());
+    }
+    
+    @Test
+    public void ottaminenToimii() {
+        kortti.otaRahaa(10);
+        assertEquals("saldo: 0.0", kortti.toString());
+    }
+    
+    @Test
+    public void liikaaOttaminenEiToimi() {
+        kortti.otaRahaa(11);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    @Test
+    public void otaRahaaPalauttaaTrueKunOttaminenOnnistuu() {
+        assertTrue(kortti.otaRahaa(10));
+    }
+
+    @Test
+    public void otaRahaaPalauttaaFalseKunOttaminenEiOnnistu() {
+        assertFalse(kortti.otaRahaa(11));
+    }
 }
