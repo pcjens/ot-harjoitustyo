29040ac7957ea77673648bfc5f2fff37479da66a
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2019-05-05 22:30:46 +0300

    Bump version to 1.0

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ mvn test
 mvn test jacoco:report
 mvn jxr:jxr checkstyle:checkstyle
 mvn package
-cp target/Roguesque-1.0-RC01.jar .
-zip Roguesque-1.0-RC01.zip Roguesque-1.0-RC01.jar items.csv
-rm Roguesque-1.0-RC01.jar
+cp target/Roguesque-1.0.jar .
+zip Roguesque-1.0.zip Roguesque-1.0.jar items.csv
+rm Roguesque-1.0.jar
